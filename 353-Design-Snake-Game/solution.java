d0e91fbff51a7d340fd1d841daf3cc0a422bf102
always414 <yichenzhang14@gmail.com>
2016-06-07 01:00:15 -0400

    [353. Design Snake Game][Runtime Error]committed by LeetCode Extension

--- 353-Design-Snake-Game/solution.java
+++ 353-Design-Snake-Game/solution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
@@ -6,22 +7,20 @@ import java.util.Queue;
 
 public class SnakeGame {
 
-	private int[][] board;
-	private int score;
-	private int length;
 	private Queue<int[]> foodQue;
 	private Deque<int[]> snake;
 
 	private static final Map<String, int[]> moveCommand;
-
 	static {
 		moveCommand = new HashMap<>();
 		moveCommand.put("U", new int[] { -1, 0 });
-		moveCommand.put("D", new int[] { 1, 0});
+		moveCommand.put("D", new int[] { 1, 0 });
 		moveCommand.put("L", new int[] { 0, -1 });
 		moveCommand.put("R", new int[] { 0, 1 });
 	}
 
+	private final int width;
+	private final int height;
 	private static final int X = 0;
 	private static final int Y = 1;
 	private static final int FOOD = Integer.MAX_VALUE;
@@ -38,8 +37,9 @@ public class SnakeGame {
 	 *            first food is positioned at [1,1], the second is at [1,0].
 	 */
 	public SnakeGame(int width, int height, int[][] food) {
-		board = new int[height][width];
-
+		this.width = width;
+		this.height = height;
+		
 		foodQue = new ArrayDeque<>();
 		for (int[] pos : food) {
 			foodQue.add(pos);
@@ -48,13 +48,6 @@ public class SnakeGame {
 		snake = new ArrayDeque<>();
 		snake.add(new int[] { 0, 0 });
 
-		score = 0;
-		length = 1;
-		initialize();
-	}
-
-	private void initialize() {
-		board[0][0] = length;
 		refreshFood();
 	}
 
@@ -68,83 +61,59 @@ public class SnakeGame {
 	 */
 	public int move(String direction) {
 		if (!moveCommand.containsKey(direction)) {
-			System.out.println("Error - invalid command");
 			return -1;
 		}
 
 		int[] move = moveCommand.get(direction);
-		int[] headPos = snake.peek();
-		int[] newHead = new int[] { move[X] + headPos[X], move[Y] + headPos[Y] };
+		int[] oldHead = snake.peek();
+		int[] newHead = new int[] { move[X] + oldHead[X], move[Y] + oldHead[Y] };
 
 		if (!inBound(newHead)) {
-			System.out.println("Game over, collide with bound!");
+			// System.out.println("Game over, collide with bound!");
 			return -1;
 		}
 
 		// a valid move
 		if (getOccupation(newHead) == 0) {
-			writeBoard(newHead, length);
-			
-			//for every body part, overwrite board to its val - 1
-			// at last, if > 0, enque
-			for (int i = 1; i <= length; i++) {
-				int[] body = snake.poll();
-				int val = Math.max(0, length - i);
-				writeBoard(body, val);
-				if (val > 0) {
-					snake.add(body);
-				}
-			}
-			
+			snake.removeLast();
 			snake.addFirst(newHead);
 		}
 
 		// eat a food
 		else if (getOccupation(newHead) == FOOD) {
-			score++;
-			length++;
-			writeBoard(newHead, length);
-			System.out.println("Eat");
 			snake.addFirst(newHead);
-			// update food
 			refreshFood();
 		}
 
 		// eat self
 		else {
-			System.out.println("Game over! Eat self.");
+			assert (getOccupation(newHead) == -1);
 			return -1;
 		}
-//		System.out.println(Arrays.deepToString(board));
-		return score;
+		
+		return snake.size()-1;
 	}
 
 	private boolean refreshFood() {
-		
-		while (!foodQue.isEmpty()) {
-			while (getOccupation(foodQue.peek()) != 0) {
-				foodQue.poll();
-			}
-			break;
+		while ((!foodQue.isEmpty()) && (!inBound(foodQue.peek()) || getOccupation(foodQue.peek()) != 0)) {
+			foodQue.poll();
 		}
-		if (foodQue.isEmpty()) {
-			return false;
-		} else {
-			writeBoard(foodQue.poll(), FOOD);
-			return true;
-		}
+		return !foodQue.isEmpty();
 	}
 
-	private void writeBoard(int[] pos, int val) {
-		board[pos[X]][pos[Y]] = val;
-	}
-
+	
 	private boolean inBound(int[] pos) {
-		return pos[X] >= 0 && pos[Y] >= 0 && pos[X] < board.length && pos[Y] < board[0].length;
+		return pos[X] >= 0 && pos[Y] >= 0 && pos[X] < height && pos[Y] < width;
 	}
 
 	private int getOccupation(int[] pos) {
-		return board[pos[X]][pos[Y]];
+		if (snake.contains(pos)) {
+			return -1;
+		} else if (foodQue.peek().equals(pos)) {
+			return FOOD;
+		} else {
+			return 0;
+		}
 	}
 
 }
