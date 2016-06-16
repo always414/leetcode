bbe296b0839165675d9995c2edc2deb463722603
always414 <yichenzhang14@gmail.com>
2016-06-16 16:29:16 -0400

    [21. Merge Two Sorted Lists][Accepted]committed by Me

---
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode pointer = dummy;
+        while (l1 != null && l2 != null) {
+        	if (l1.val <= l2.val) {
+        		pointer.next = l1;
+        		pointer = pointer.next;
+        		l1 = l1.next;
+        	} else {
+        		pointer.next = l2;
+        		pointer = pointer.next;
+        		l2 = l2.next;
+        	}
+        }
+        
+        if (l1 != null) {
+        	pointer.next = l1;
+        }
+        
+        if (l2 != null) {
+        	pointer.next = l2;
+        }
+        
+        return dummy.next;
+    }
+}
