efbaa9083cd1084ec932dbd8488f00529e79f437
always414 <yichenzhang14@gmail.com>
2016-06-16 20:07:03 -0400

    [234. Palindrome Linked List][Runtime Error]committed by Me

---
+++ 234-Palindrome-Linked-List/solution.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // reverse half list and compare
+    	// O(cn) time and O(1) space
+    	ListNode middle = findMiddle(head);
+    	
+    	ListNode left = head;
+    	ListNode right = middle.next;
+    	middle.next = null; // break the list into halves
+    	right = reverse(right);
+    	
+    	while (left != null) {
+    		if (left.val != right.val) {
+    			return false;
+    		} 
+    	}
+    	
+    	return true;
+    }
+    
+    private ListNode reverse(ListNode head) {
+    	ListNode res = null;
+    	while (head != null) {
+    		ListNode next = head.next;
+    		head.next = res;
+    		res = head;
+    		head = next;
+    	}
+    	return res;
+    }
+    
+    private ListNode findMiddle (ListNode head) {
+    	ListNode fast = head, slow = head;
+    	while (fast != null) {
+    		if (fast.next != null && fast.next.next != null) {
+    			fast = fast.next.next;
+    			slow = slow.next;
+    		} else {
+    			
+    		}
+    	}
+    	
+    	return slow;
+    }
+}
