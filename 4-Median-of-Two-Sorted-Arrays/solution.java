73e879987487939c5dea9f66dc95ea59bd7d58e3
always414 <yichenzhang14@gmail.com>
2016-06-17 15:22:27 -0700

    [4. Median of Two Sorted Arrays][Runtime Error]committed by Me

---
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int[] num = merge(nums1, nums2);
+		int length = num.length;
+		if (length % 2 == 0) {
+			return (num[length/2] + num[length/2 + 1])/2;
+		} else {
+			return num[(length / 2 + 1)];
+		}
+	}
+
+	private int[] merge(int[] nums1, int[] nums2) {
+		int[] num = new int[nums1.length + nums2.length];
+		int i = 0, j = 0, k = 0;
+
+		while (k < num.length) {
+			if (i == nums1.length && j < nums2.length) {
+				num[k++] = nums2[j++];
+			} else if (j == nums2.length && i < nums1.length) {
+				num[k++] = nums1[i++];
+			} else if (i < nums1.length && j < nums2.length) {
+				if (nums1[i] <= nums2[j]) {
+					num[k++] = nums1[i++];
+				} else {
+					num[k++] = nums2[j++];
+				}
+			} else {
+				break;
+			}
+		}
+		return num;
+	}
+
+}
