a71ddbbbfd0ad3da195706139b962b8705a303d1
always414 <yichenzhang14@gmail.com>
2016-06-17 17:27:08 -0400

    [61. Rotate List][Accepted]committed by Me

--- 61-Rotate-List/solution.java
+++ 61-Rotate-List/solution.java
@@ -1,49 +1,46 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
  */
 public class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-    	if (head == null) {
-    		return null;
-    	}
-    	if (k == 0) {
-    		return head;
-    	}
-    	
-    	ListNode dummy = new ListNode(0);
-    	
-        ListNode fast = head, slow = head;
-        
-        while (k > 0) {
-        	fast = fast.next;
-        	k--;
-        	// deal with some corner cases
-        	if (fast == null && k == 0) {
-        		return head;
-        	}
-        	
-        	if (fast == null && k > 0) {
-        		fast = head;
-        	}
-        }
-        
-       
+	public ListNode rotateRight(ListNode head, int k) {
+		if (head == null) {
+			return null;
+		}
+		if (k == 0 || head.next == null) {
+			return head;
+		}
 
-        while (fast.next != null) {
-        	fast = fast.next;
-        	slow = slow.next;
-        }
-        
-        dummy.next = slow.next;
-        slow.next = null;
-        fast.next = head;
-        
-        return dummy.next;
-    	
-    }
+		ListNode dummy = new ListNode(0);
+
+		ListNode fast = head, slow = head;
+		int length = 0;
+		while (k > 0) {
+			fast = fast.next;
+			length++;
+			k--;
+			if (fast == null) {
+				// reach to the end, get the length, first mode k
+				// then decide
+				k %= length;
+				if (k == 0) {
+					return head;
+				} else {
+					fast = head;
+				}
+			}
+		}
+
+		while (fast.next != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+
+		dummy.next = slow.next;
+		slow.next = null;
+		fast.next = head;
+
+		return dummy.next;
+
+	}
 }
