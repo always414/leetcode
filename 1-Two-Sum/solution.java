04096a3b3e7d36136091587e75638131cee21170
always414 <yichenzhang14@gmail.com>
2016-06-17 14:48:26 -0700

    [1. Two Sum][Accepted]committed by Me

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -12,18 +13,31 @@ public class Solution {
     		}
     	}*/
     	
+    	Map<Integer, Integer> map = new HashMap<>();
+
     	// two way hashmap solution O(n) time O(n) space
-    	HashMap<Integer, Integer> map = new HashMap<>();
-    	for (int i = 0; i < nums.length; i++) {
+    	/*for (int i = 0; i < nums.length; i++) {
     		map.put(nums[i], i);
     	}
     	
     	for (int i = 0; i < nums.length; i++) {
     		int complement = target - nums[i];
+    		// situation that i appear double times
     		if (map.containsKey(complement) && map.get(complement) != i) {
     			return new int[] {map.get(complement), i};
     		}
+    	}*/
+    	
+    	//one way hashmap solution, o(n) time O(n) space
+    	//brilliant
+    	for (int i = 0; i < nums.length; i++) {
+    		int complement = target - nums[i];
+    		if (map.containsKey(complement)) {
+    			return new int[] {map.get(complement), i};
+    		}
+    		map.put(nums[i], i);
     	}
+    	
     	
     	throw new IllegalArgumentException("no two sum solution") ;
     	
