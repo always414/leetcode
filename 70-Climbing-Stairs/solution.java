284f19c628caf8405a7b6b498313bca8938568f6
always414 <yichenzhang14@gmail.com>
2016-06-21 21:17:17 -0700

    [70. Climbing Stairs][Accepted]committed by Me

--- 70-Climbing-Stairs/solution.java
+++ 70-Climbing-Stairs/solution.java
@@ -1,11 +1,15 @@
 public class Solution {
     public int climbStairs(int n) {
-        int[] matrix = new int[n+1];
+        if (n < 3) {
+        	return n;
+        }
+    	int[] matrix = new int[n+1];
         //OPT(i) = OPT(i-1) + OPT(i-2)
         
         matrix[0] = 0;
         matrix[1] = 1;
         matrix[2] = 2;
+        
         
         for (int i = 3; i <= n; i++) {
         	matrix[i] = matrix[i-1] + matrix[i-2];
