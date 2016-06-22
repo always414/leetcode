99458457e58734439fd05804705f068d839bc7b4
always414 <yichenzhang14@gmail.com>
2016-06-22 15:16:36 -0700

    [120. Triangle][Runtime Error]committed by Me

---
+++ 120-Triangle/solution.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+    	if (triangle == null || triangle.size() == 0) {
+    		return 0;
+    	}
+    	if (triangle.size() == 1 ) {
+    		return triangle.get(0).get(0);
+    	}
+    	
+        List<Integer> result = new ArrayList<>(triangle.size());
+        result.set(0, triangle.get(0).get(0));
+        
+        for (int i = 1; i < triangle.size(); i++) {
+        	for (int j = 0; j <= i; j++) {
+        		if (j == 0) {
+        			result.set(j, triangle.get(i - 1).get(j) + triangle.get(i).get(j)); 
+        		} else {
+        			result.set(j, triangle.get(i).get(j) + Math.min(triangle.get(i-1).get(j), triangle.get(i-1).get(j-1)));
+        		}
+        	}
+        }
+        return Collections.min(result);
+    }
+}
