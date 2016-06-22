fa3cf0f660d4027565a56592e75c424e52103d96
always414 <yichenzhang14@gmail.com>
2016-06-21 22:11:01 -0700

    [198. House Robber][Accepted]committed by Me

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -1,18 +1,23 @@
-import java.util.Arrays;
-
 public class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        if(nums.length == 1) return nums[0];
-    	int[] dp = new int[nums.length];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-
-        for(int i = 2;i<nums.length;i++){
-            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);           
-        }
-    	System.out.println(Arrays.toString(dp));
-        return dp[nums.length-1];
+       // matrix[i] represents the maximum amount of money can rob at point i
+    	// matrix[i] = nums[i] +  matrix[i-2]
+    	if (nums == null || nums.length == 0) {
+    		return 0;
+    	}
+    	
+    	if (nums.length == 1) {
+    		return nums[0];
+    	}
+    	
+    	int[] matrix = new int[nums.length];
+    	matrix[0] = nums[0];
+    	matrix[1] = Math.max(nums[0], nums[1]);
+    	for (int i = 2; i < nums.length; i++) {
+    		matrix[i] = Math.max(matrix[i-1], matrix[i-2] + nums[i]); // max rob i or not
+    	}
+        
+        return matrix[matrix.length - 1];
+    	
     }
 }
