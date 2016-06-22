24fcfa3cbcb10b158c7bbc0d2af1091816500f8e
always414 <yichenzhang14@gmail.com>
2016-06-21 21:15:59 -0700

    [70. Climbing Stairs][Runtime Error]committed by Me

---
+++ 70-Climbing-Stairs/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int climbStairs(int n) {
+        int[] matrix = new int[n+1];
+        //OPT(i) = OPT(i-1) + OPT(i-2)
+        
+        matrix[0] = 0;
+        matrix[1] = 1;
+        matrix[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+        	matrix[i] = matrix[i-1] + matrix[i-2];
+        }
+        
+        return matrix[n];
+    }
+} 
