5b2edcf10a6cb1e021dbd20efd2b28a5a9ef5067
always414 <yichenzhang14@gmail.com>
2016-06-22 14:05:59 -0700

    [91. Decode Ways][Accepted]committed by Me

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,14 +1,26 @@
 public class Solution {
     public int numDecodings(String s) {
+    	if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+    		return 0;
+    	}
+    	
         int[] matrix = new int[s.length() + 1];
         matrix[0] = 1;
         matrix[1] = 1;
-        
         for (int i = 2; i < s.length() + 1; i++) {
-        	matrix[i] = matrix[i-1];
-        	if (Integer.parseInt(s.substring(i-2, i)) <= 26) {
+        	int single = Integer.parseInt(s.substring(i-1, i));
+        	int two = Integer.parseInt(s.substring(i-2, i));
+        	if (single == 0 && (two >26 || two < 1)) {
+        		return 0;
+        	}
+        	
+        	if (single != 0) {
+        		matrix[i] = matrix[i-1];
+        	}
+        	
+        	if (two >= 10 && two <= 26) {
         		matrix[i] += matrix[i-2];
-        	};
+        	}
         }
         return matrix[s.length()];
     }
