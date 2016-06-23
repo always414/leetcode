cf99dc73cc49e3684bb45c7a5286b65302b2968f
always414 <yichenzhang14@gmail.com>
2016-06-23 14:54:20 -0700

    [304. Range Sum Query 2D - Immutable][Time Limit Exceeded]committed by Me

---
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -0,0 +1,22 @@
+public class NumMatrix {
+	int[][] matrix;
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+        	for (int j = col1; j <= col2; j++) {
+        		sum += matrix[i][j];
+        	}
+        }
+        return sum;
+    }
+}
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix = new NumMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.sumRegion(1, 2, 3, 4);
