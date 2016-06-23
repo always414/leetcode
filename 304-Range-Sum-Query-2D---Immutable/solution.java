114e03afdf5d6101a5c22729f180b4ad79bd4781
always414 <yichenzhang14@gmail.com>
2016-06-23 15:13:07 -0700

    [304. Range Sum Query 2D - Immutable][Runtime Error]committed by Me

--- 304-Range-Sum-Query-2D---Immutable/solution.java
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -1,56 +1,52 @@
 public class NumMatrix {
 	int[][] sum;
-    public NumMatrix(int[][] matrix) {
-    	if (matrix == null || matrix.length == 0) {
-    		sum = null;
-    	}
-       sum = new int[matrix.length][matrix[0].length];
-       sum[0][0] = matrix[0][0];
-       for (int i = 1; i < matrix.length; i++) {
-    	   sum[i][0] = matrix[i][0] + sum[i-1][0];
-       }
-       
-       for (int j = 1; j < matrix[0].length; j++) {
-    	   sum[0][j] = matrix[0][j] + sum[0][j-1];
-       }
-       
-       for (int i = 1; i < matrix.length; i++) {
-    	   for (int j = 1; j < matrix[0].length; j++) {
-    		   sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i][j];
-    	   }
-       }
-    }
 
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-    	if (sum == null) {
-    		return 0;
-    	}
-    	if (!isValid(row1, col1, row2, col2)) {
-    		return 0;
-    	}
-    	
-    	if (row1 == 0 && col2 == 0) {
-    		return sum[row2][col2];
-    	}
-    	
-    	if (row1 == 0) {
-    		return sum[row2][col2] - sum[row2][col1-1];
-    	}
-    	
-    	if (col1 == 0) {
-    		return sum[row2][col2] - sum[row1-1][col2];
-    	}
-    	
-    	return sum[row2][col2] - sum[row2][col1-1] - sum[row1-1][col2] + sum[row1-1][col1-1];
-    }
-    
-    private boolean isValid(int row1, int col1, int row2, int col2) {
-    	return (row1 >= 0 && row2 >= 0 && col1 >= 0 && col2 >= 0) 
-    			&& (row1 <= row2 && col1 <= col2) 
-    			&& (row2 < sum.length && col1 < sum[0].length);
-    }
+	public NumMatrix(int[][] matrix) {
+		if (matrix == null || matrix.length == 0) {
+			throw new IllegalArgumentException("invalid input");
+		}
+		sum = new int[matrix.length][matrix[0].length];
+		sum[0][0] = matrix[0][0];
+		for (int i = 1; i < matrix.length; i++) {
+			sum[i][0] = matrix[i][0] + sum[i - 1][0];
+		}
+
+		for (int j = 1; j < matrix[0].length; j++) {
+			sum[0][j] = matrix[0][j] + sum[0][j - 1];
+		}
+
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length; j++) {
+				sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i][j];
+			}
+		}
+	}
+
+	public int sumRegion(int row1, int col1, int row2, int col2) {
+		if (!isValid(row1, col1, row2, col2)) {
+			return 0;
+		}
+
+		if (row1 == 0 && col2 == 0) {
+			return sum[row2][col2];
+		}
+
+		if (row1 == 0) {
+			return sum[row2][col2] - sum[row2][col1 - 1];
+		}
+
+		if (col1 == 0) {
+			return sum[row2][col2] - sum[row1 - 1][col2];
+		}
+
+		return sum[row2][col2] - sum[row2][col1 - 1] - sum[row1 - 1][col2] + sum[row1 - 1][col1 - 1];
+	}
+
+	private boolean isValid(int row1, int col1, int row2, int col2) {
+		return (row1 >= 0 && row2 >= 0 && col1 >= 0 && col2 >= 0) && (row1 <= row2 && col1 <= col2)
+				&& (row2 < sum.length && col1 < sum[0].length);
+	}
 }
-
 
 // Your NumMatrix object will be instantiated and called as such:
 // NumMatrix numMatrix = new NumMatrix(matrix);
