c2cddee028f861c4a67b7563dfad43462762554c
always414 <yichenzhang14@gmail.com>
2016-06-24 15:31:56 -0700

    [87. Scramble String][Accepted]committed by Me

--- 87-Scramble-String/solution.java
+++ 87-Scramble-String/solution.java
@@ -8,7 +8,7 @@ public class Solution {
             letters[s1.charAt(i)-'a']++;
             letters[s2.charAt(i)-'a']--;
         }
-		
+		 for (int i=0; i<26; i++) if (letters[i]!=0) return false;
 		for (int i = 1; i < s1.length(); i++) {
 			// compare every substring from (0,i) (i,length)
 			// if true, return result;
