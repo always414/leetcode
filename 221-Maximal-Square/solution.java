b8675923339559730a513d311729747198a76c44
always414 <yichenzhang14@gmail.com>
2016-06-23 18:39:42 -0700

    [221. Maximal Square][Accepted]committed by Me

---
+++ 221-Maximal-Square/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length== 0) {
+        	return 0;
+        }
+    	int result = 0;
+    	int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+    
+    	for (int i = 1; i < dp.length; i++) {
+    		for (int j = 1; j < dp[0].length; j++) {
+    			if (matrix[i-1][j-1] == '1') {
+    				dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+    				result = Math.max(result, dp[i][j]);
+    			}
+    		}
+    	}
+    	
+    	// dp the length not the area square
+    	return result * result;
+    }
+}
