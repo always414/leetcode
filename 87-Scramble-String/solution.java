975a5ec57e67d571185d8bb668aa11b2c95434be
always414 <yichenzhang14@gmail.com>
2016-06-24 15:25:17 -0700

    [87. Scramble String][Time Limit Exceeded]committed by Me

---
+++ 87-Scramble-String/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+	public boolean isScramble(String s1, String s2) {
+		if (s2.equals(s1)) {
+			return true;
+		}
+
+		for (int i = 1; i < s1.length(); i++) {
+			// compare every substring from (0,i) (i,length)
+			// if true, return result;
+			if (isScramble(s1.substring(0, i), s2.substring(0, i)) 
+					&& isScramble(s1.substring(i), s2.substring(i))) {
+				return true;
+			}
+			if (isScramble(s1.substring(0, i), s2.substring(s2.length() - i)) 
+					&& isScramble(s1.substring(i), s2.substring(0, s2.length() - i))) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
