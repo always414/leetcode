8e074beb0e2c2bd6bd8b68e47e2d95e49ae47e17
always414 <yichenzhang14@gmail.com>
2016-06-27 12:36:49 -0700

    [36. Valid Sudoku][Accepted]committed by Me

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -1,36 +1,30 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Solution {
 	public boolean isValidSudoku(char[][] board) {
-		boolean[] row = new boolean[10];
-		boolean[] col = new boolean[10];
-		boolean[] cube = new boolean[10];
 
 		for (int i = 0; i < 9; i++) {
-			Arrays.fill(row, false); // reset row for every updating i;
-			Arrays.fill(col, false);
-			Arrays.fill(cube, false);
+			HashSet<Character> row = new HashSet<>();
+			HashSet<Character> col = new HashSet<>();
+			HashSet<Character> cube = new HashSet<>();
+			
 			for (int j = 0; j < 9; j++) {
-				if (board[i][j] != '.') {
-					int num = board[i][j] - '0';
-					if (row[num])
+				if (board[i][j] != '.' && !row.add(board[i][j])) {
 						return false;
-					row[num] = true; // set number to true;
 				}
 				
-				if (board[j][i] != '.') {
-					int num = board[j][i] - '0';
-					if (col[num]) return false;
-					col[num] = true;
+				if (board[j][i] != '.' && !col.add(board[j][i])) {
+					return false;
 				}
+				
 				
 				int rowIndex = 3 * (i/3);//0,0,0,1,1,1,2,2,2
 				int colIndex = 3 * (i%3);//0,1,2,0,1,2,0,1,2
 				
-				if (board[rowIndex + j/3][colIndex + j%3] != '.') {
-					int num = board[rowIndex + j/3][colIndex + j%3] - '0';
-					if (cube[num]) return false;
-					cube[num] = true;
+				if (board[rowIndex + j/3][colIndex + j%3] != '.'
+						&& !cube.add(board[rowIndex + j/3][colIndex + j%3])) {
+					return false;
 				}
 			}
 		}
