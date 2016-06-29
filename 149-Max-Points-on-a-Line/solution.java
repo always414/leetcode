f69879c1832745ea9f57f37a23ff175f69daeac1
always414 <yichenzhang14@gmail.com>
2016-06-28 17:06:36 -0700

    [149. Max Points on a Line][Runtime Error]committed by Me

---
+++ 149-Max-Points-on-a-Line/solution.java
@@ -0,0 +1,43 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.print.attribute.HashAttributeSet;
+
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution {
+    public int maxPoints(Point[] points) {
+    	if (points == null) {
+    		return 0;
+    	}
+    	if (points.length <= 2) {
+    		return points.length;
+    	}
+    	int result = 0;
+    	Map<Integer, Integer> map = new HashMap<>();
+    	for (int i = 0; i < points.length - 1; i++) {
+    		int overlap = 0;
+    		for (int j = i + 1; j < points.length; j++) {
+    			if (points[i].x == points[j].x && points[i].y ==points[j].y) {
+    				overlap ++;
+    			} else {
+    				double k = (points[j].y - points[i].y) / (points[j].x - points[i].x);
+    				double c = points[i].y - points[i].x * k;
+    				int hashVal = Objects.hash(k, c);
+    				map.put(hashVal, map.getOrDefault(hashVal, 0) + 1);
+    			}
+    		}
+    		result = Math.max(result, Collections.max(map.values()) + overlap + 1);
+    	}
+    	return result;
+    }
+}
