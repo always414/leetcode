e1f1c4db14a84e7beccb3438b6980bcf09e0ce84
always414 <yichenzhang14@gmail.com>
2016-07-09 10:15:04 -0700

    [101. Symmetric Tree][Runtime Error]committed by Me

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -1,3 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -9,7 +13,34 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return root == null || symmetricHelper(root.left, root.right);
+    	//iterative solution
+    	
+    	Queue<TreeNode> queue = new ArrayDeque<>();
+    	if (root == null) return true;
+    	
+    	queue.add(root.left);
+    	queue.add(root.right);
+    	
+    	while (queue.size() > 1 ) {
+    		TreeNode left = queue.poll(),
+    				right = queue.poll();
+    		if (left == null || right == null) {
+    			return left == right;
+    		}
+    		
+    		if (left.val != right.val ) {
+    			return false;
+    		}
+    		
+    		queue.add(left.left);
+    		queue.add(right.right);
+    		queue.add(left.right);
+    		queue.add(right.left);
+    	}
+    	
+    	return true;
+    	
+//        return root == null || symmetricHelper(root.left, root.right);
     }
     
     //recursion solution 
