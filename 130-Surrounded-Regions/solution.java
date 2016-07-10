976ab9740dd63002300908b99c01442017c75877
always414 <yichenzhang14@gmail.com>
2016-07-09 17:41:59 -0700

    [130. Surrounded Regions][Runtime Error]committed by Me

---
+++ 130-Surrounded-Regions/solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+	private static int[] I = {0, 0, 1, -1};
+	private static int[] J = {1, -1, 0, 0};
+	
+    public void solve(char[][] board) {
+        for (int i = 0; i < board.length; i += board.length - 1) {
+        	for (int j = 0; j < board[0].length; j++) {
+        		paintHelper(board, i, j, 'B');
+        	}
+        }
+        
+        for (int j = 0; j < board[0].length; j+= board[0].length - 1) {
+        	for (int i = 0; i < board.length; i++) {
+        		paintHelper(board, i, j, 'B');
+        	}
+        }
+        
+        for (int i = 0; i < board.length; i++) {
+        	for (int j = 0; j < board[0].length; j++) {
+        		if (board[i][j] == 'O') {
+        			board[i][j] = 'X';
+        		} else if (board[i][j] == 'B') {
+        			board[i][j] = 'O';
+        		}
+        	}
+        }
+    	
+    }
+    
+    private void paintHelper(char[][] board, int i, int j, char c) {
+    	if (i < 0 || j < 0 || i > board.length || j > board[0].length || board[i][j] == 'X') {
+    		return;
+    	}
+    	
+    	board[i][j] = c;
+    	
+    	for (int k = 0; k < 4; k++) {
+    		paintHelper(board, i + I[k], j + J[k], c);
+    	}
+    }
+}
