a67d9955f3881339267019cf1261e25b2d167320
always414 <yichenzhang14@gmail.com>
2016-07-09 17:57:54 -0700

    [130. Surrounded Regions][Runtime Error]committed by Me

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -37,7 +37,7 @@ public class Solution {
     }
     
     private void paintHelper(char[][] board, int i, int j, char c) {
-    	if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == 'X') {
+    	if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == 'X' || board[i][j] == 'B') {
     		return;
     	}
     	
