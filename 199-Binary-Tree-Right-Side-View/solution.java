48d45657d10b80f7e667f7588fb6d8fd9c798a9d
always414 <yichenzhang14@gmail.com>
2016-07-09 20:46:43 -0700

    [199. Binary Tree Right Side View][Accepted]committed by Me

--- 199-Binary-Tree-Right-Side-View/solution.java
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -4,43 +4,38 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 public class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) {
-        	return new ArrayList();
-        }
-        List<Integer> res = new ArrayList<>();
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
-        
-        while (! queue.isEmpty()) {
-        	int size = queue.size();
-        	while (size > 0) {
-        		if (queue.peek().left != null) {
-        			queue.offer(queue.peek().left);
-        		}
-        		if (queue.peek().right != null) {
-        			queue.offer(queue.peek().right);
-        		}
-        		
-        		if (size == 1) {
-        			res.add(queue.poll().val);
-        		} else {
-        			queue.poll();
-        		}
-        		size --;
-        	}
-        	
-        }
-        return res;
-    	
-    }
+	public List<Integer> rightSideView(TreeNode root) {
+		if (root == null) {
+			return new ArrayList();
+		}
+		List<Integer> res = new ArrayList<>();
+		Queue<TreeNode> queue = new ArrayDeque<>();
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			while (size-- > 0) {
+				if (queue.peek().left != null) {
+					queue.offer(queue.peek().left);
+				}
+				if (queue.peek().right != null) {
+					queue.offer(queue.peek().right);
+				}
+
+				if (size == 0) {
+					res.add(queue.poll().val);
+					break;
+				}
+
+				queue.poll();
+			}
+
+		}
+		return res;
+
+	}
 }
