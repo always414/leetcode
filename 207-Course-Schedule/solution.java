9490f45c20524ee5d72e10b23339cbf617807ac9
always414 <yichenzhang14@gmail.com>
2016-07-10 08:50:49 -0700

    [207. Course Schedule][Accepted]committed by Me

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,38 +1,46 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 
 public class Solution {
 
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
-		int[][] matrix = new int[numCourses][numCourses];
-		int[] indegree = new int[numCourses];
+		List<List<Integer>> graph = new ArrayList<>();
+		
+
+		int[] degree = new int[numCourses];
+		Queue<Integer> queue = new ArrayDeque<>();
+		int count = 0;
+		
+		for (int i = 0; i < numCourses;i++) {
+			graph.add(new ArrayList<>());
+		}
 
 		for (int i = 0; i < prerequisites.length; i++) {
-			int ready = prerequisites[i][0];
-			int pre = prerequisites[i][1];
-			if (matrix[pre][ready] == 0) {
-				indegree[ready]++;
-			}
-			matrix[pre][ready] = 1;
+			degree[prerequisites[i][1]] ++;
+			graph.get(prerequisites[i][0]).add(prerequisites[i][1]);
 		}
-
-		int count = 0;
-		Queue<Integer> queue = new ArrayDeque<>();
-		for (int i = 0; i < indegree.length; i++) {
-			if (indegree[i] == 0) {
-				queue.offer(i);
+		
+		for (int i = 0; i < degree.length; i++) {
+			if (degree[i] == 0) {
+				queue.add(i);
+				count ++;
 			}
 		}
 
-		while (!queue.isEmpty()) {
-			int course = queue.poll();
-			count++;
-			for (int i = 0; i < numCourses; i++) {
-				if (matrix[course][i] != 0 && --indegree[i] == 0) {
-					queue.offer(i);
+		while (! queue.isEmpty()) {
+			int course =  queue.poll();
+			for (int i = 0; i < graph.get(course).size(); i++) {
+				int pointer = graph.get(course).get(i);
+				degree[pointer] --;
+				if (degree[pointer] == 0) {
+					queue.add(pointer);
+					count ++;
 				}
 			}
 		}
+		
 		return count == numCourses;
 	}
 }
