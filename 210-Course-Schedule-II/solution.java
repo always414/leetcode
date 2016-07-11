e24cd072ea076ff715680dc847f0ffef80841020
always414 <yichenzhang14@gmail.com>
2016-07-11 16:42:55 -0700

    [210. Course Schedule II][Accepted]committed by Me

--- 210-Course-Schedule-II/solution.java
+++ 210-Course-Schedule-II/solution.java
@@ -1,3 +1,4 @@
+import java.nio.file.ProviderNotFoundException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -8,33 +9,38 @@ import java.util.Set;
 public class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
        int[] order = new int[numCourses];
+       int[] preCount = new int[numCourses];
        int num = 0;
-        List<Set<Integer>> pre = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-        	pre.add(new HashSet<>());
-        }
-        for (int i = 0; i < prerequisites.length; i++) {
-        	pre.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        }
-        Queue<Integer> queue = new ArrayDeque<>();
-        for (int i = 0; i < pre.size(); i++) {
-        	if (pre.get(i).isEmpty()) {
-        		queue.add(i);
-        	}
-        }
-        while (! queue.isEmpty()) {
-//        	System.out.println(queue);
-        	Integer course = queue.poll();
-        	order[num++] = course;
-        	for (int i = 0; i < pre.size(); i++) {
-//        		System.out.println(pre.get(i).remove(course));
-        		if (pre.get(i).remove(course)) {
-        			if (pre.get(i).isEmpty()){
-        				queue.add(i);
-        			}
-        		}
-        	}
-        }
+       
+       for (int i = 0; i < prerequisites.length; i++) {
+    	   int course = prerequisites[i][0];
+    	   int pre = prerequisites[i][1];
+    	   preCount[course] ++;
+       }
+
+       Queue<Integer> queue = new ArrayDeque<>();
+       for (int i = 0; i < preCount.length; i++) {
+    	   if (preCount[i] == 0) {
+    		   queue.add(i);
+    		   preCount[i] = Integer.MAX_VALUE;
+    	   }
+       }
+       
+       while (!queue.isEmpty()) {
+    	   int preCourse = queue.poll();
+    	   order[num++] = preCourse;
+    	   for (int i = 0; i < prerequisites.length; i++) {
+    		   if (prerequisites[i][1] == preCourse) {
+    			   preCount[prerequisites[i][0]] --;
+    			   if (preCount[prerequisites[i][0]] == 0) {
+    				   queue.add(prerequisites[i][0]);
+    			   }
+    		   }
+    	   }
+       }
+       
+       
+
         return num == numCourses? order : new int[0];
     }
 }
