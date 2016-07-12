a3e4243b2c1bf91793fbe91280bcb9cadead9225
always414 <yichenzhang14@gmail.com>
2016-07-12 13:44:44 -0700

    [286. Walls and Gates][Accepted]committed by Me

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -1,47 +1,46 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-
+import java.util.*;
+class Location{
+    int i;
+    int j;
+    public Location(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
 public class Solution {
-	public void wallsAndGates(int[][] rooms) {
-		if (rooms == null || rooms.length == 0) return;
-		int width = rooms[0].length;
-		for (int i = 0; i < rooms.length; i++) {
-			for (int j = 0; j < rooms[0].length; j++) {
-					if (rooms[i][j] == 0) {
-
-						Queue<Integer> queue = new ArrayDeque<>();
-						queue.add(i * width + j);
-
-						while (!queue.isEmpty()) {
-							int index = queue.poll();
-							int row = index / width;
-							int col = index % width;
-
-							if (row - 1 >= 0 && rooms[row - 1][col] > rooms[row][col]) {
-								rooms[row - 1][col] = rooms[row][col] + 1;
-								queue.add(index - width);
-							}
-
-							if (row + 1 < rooms.length && rooms[row + 1][col] > rooms[row][col]) {
-								rooms[row + 1][col] = rooms[row][col] + 1;
-								queue.add(index + width);
-							}
-
-							if (col - 1 >= 0 && rooms[row][col - 1] > rooms[row][col]) {
-								rooms[row][col - 1] = rooms[row][col] + 1;
-								queue.add(index - 1);
-							}
-
-							if (col + 1 < width && rooms[row][col + 1] > rooms[row][col]) {
-								rooms[row][col + 1] = rooms[row][col] + 1;
-								queue.add(index + 1);
-							}
-						}
-					}
-			}
-		}
-		
-		System.out.println(Arrays.deepToString(rooms));
-	}
+    int[] I = {1, -1, 0, 0};
+    int[] J = {0, 0, 1, -1};
+    public void wallsAndGates(int[][] rooms) {
+        Queue<Location> gates = new LinkedList<Location>();
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                if (rooms[i][j] == 0) {
+                    gates.offer(new Location(i, j));
+                }
+            }
+        }
+        int level = 0;
+        while (!gates.isEmpty()) {
+            int size = gates.size();
+            for (int i = 0; i < size; i++) {
+                Location l = gates.poll();
+                rooms[l.i][l.j] = level;
+                for (int k = 0; k < 4; k++) {
+                    int ii = l.i + I[k];
+                    int jj = l.j + J[k];
+                    if (isValid(rooms, ii, jj)) {
+                        gates.offer(new Location(ii, jj));
+                        rooms[ii][jj] = -2;
+                    }
+                }
+            }
+            level++;
+        } 
+    }
+    boolean isValid(int[][] rooms, int x, int y) {
+            if (x < 0 || y < 0 || x >= rooms.length || y >= rooms[0].length || rooms[x][y] != Integer.MAX_VALUE) {
+                return false;
+            }
+            return true;
+    }
 }
