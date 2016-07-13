d056202e1160df8b4b4e7dfe02e9cdd26b09226a
always414 <yichenzhang14@gmail.com>
2016-07-13 13:02:12 -0700

    [307. Range Sum Query - Mutable][Time Limit Exceeded]committed by Me

--- 307-Range-Sum-Query---Mutable/solution.java
+++ 307-Range-Sum-Query---Mutable/solution.java
@@ -8,27 +8,31 @@ public class NumArray {
 //		O(n) initialize
         sums = new int[nums.length];
 	    this.nums = nums;
-	    sums[0] = nums[0];
-		for (int i=1;i<nums.length;i++) {
-	        sums[i] = sums[i-1] + nums[i];
-		}
+	    if (nums.length > 0) {
+    	    sums[0] = nums[0];
+    		for (int i=1;i<nums.length;i++) {
+    	        sums[i] = sums[i-1] + nums[i];
+    		}
+	    }
     }
 	
     public void update (int i, int val) {
+        if (i >= sums.length) return;
 		int diff = val - nums[i];
+		nums[i] = val;
 		for (int x = i; x < sums.length; x++) {
 		    sums[x] += diff;
 		}
 	}
 	
     public int sumRange(int i, int j) {
-    	if ( i ==0 ) return sums[j];
-    	
+    	if ( i == 0 ) return sums[j];
     	
         return sums[j] - sums[i-1];
     }
 }
-
+//[7,2,7,2,0]
+//--> 
 
 // Your NumArray object will be instantiated and called as such:
 // NumArray numArray = new NumArray(nums);
