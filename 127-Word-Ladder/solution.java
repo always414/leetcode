68406114e33d99f34fb74c170cb1ea6e6a5c9e62
always414 <yichenzhang14@gmail.com>
2016-07-14 15:36:06 -0700

    [127. Word Ladder][Time Limit Exceeded]committed by Me

---
+++ 127-Word-Ladder/solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Set;
+
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+    	Queue<String> queue = new ArrayDeque<>();
+    	queue.add(beginWord);
+    	int count = 1;
+    	while (!queue.isEmpty()) {
+    		int size = queue.size();
+    		count ++;
+    		while (size-- > 0) {
+    			char[] charArray = queue.poll().toCharArray();
+    			for (int i = 0; i < charArray.length; i++) {
+    				char originalChar = charArray[i];
+    				for (char c = 'a'; c <= 'z'; c++) {
+    					charArray[i] = c;
+    					String newWord = String.valueOf(charArray);
+//    					System.out.println(newWord);
+    					if (newWord.equals(endWord)) {
+    						return count;
+    					}
+    					if (wordList.contains(newWord)) {
+    						queue.add(newWord);
+    					} 
+    				}
+    				charArray[i] = originalChar;
+    			}
+    		}
+    	}
+        return 0;
+    }
+}
