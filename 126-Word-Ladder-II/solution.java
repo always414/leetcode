371a318dc9c817b7a8f0b524ebd05d3e983d3930
always414 <yichenzhang14@gmail.com>
2016-07-15 12:14:08 -0700

    [126. Word Ladder II][Accepted]committed by Me

--- 126-Word-Ladder-II/solution.java
+++ 126-Word-Ladder-II/solution.java
@@ -1,85 +1,69 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
-import java.util.logging.Level;
-
-import org.omg.CORBA.TRANSACTION_MODE;
-
 public class Solution {
-    public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
-        List<List<String>> res = new ArrayList<>();
-        Queue<List<String>> queue = new ArrayDeque<>();
-        List<String> currentTransaction = new ArrayList<>(Arrays.asList(beginWord));
-        queue.add(currentTransaction);
-        
-        while (!queue.isEmpty()) {
-        	int size = queue.size();
-        	Set<String> levelWord = new HashSet<>();
-        	boolean find = false;
-
-        	while (size -- > 0) {
-        		// do level bfs 
-        		currentTransaction = queue.poll();
-        		List<List<String>> newTransactions = bfsHelper(currentTransaction, endWord, wordList, levelWord);
-
-        		for (List<String> transaction : newTransactions) {
-        			String childWord = transaction.get(transaction.size() - 1);
-        			levelWord.add(childWord);
-        			if (childWord.equals(endWord)) {
-        				res.add(transaction);
-        				find = true;
-        			}
-        		}
-        		if (!find) {
-        			queue.addAll(newTransactions);
-        		}
-        	}
-        	
-        	if (find) {
-        		break;
-        	}
-        	wordList.removeAll(levelWord); // delete level words
+    public List<List<String>> findLadders(String beginWord, String endWord, Set<String> set) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> result = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        path.add(endWord);
+        ladderLength(beginWord, endWord, set, map);
+        dfs(result, path, endWord, beginWord, map);
+        return result;
+    }
+    void dfs(List<List<String>> result, List<String> path, String endWord, String beginWord, Map<String, List<String>> map){
+        if (endWord.equals(beginWord)) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (String temp : map.get(endWord)) {
+            path.add(0, temp);
+            dfs(result, path, temp, beginWord, map);
+            path.remove(0);
         }
         
-        return res;
+    }
+    void ladderLength(String beginWord, String endWord, Set<String> set, Map<String, List<String>> map) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int level = 0;
+        set.remove(beginWord);
+        for (String key : set) {
+            map.put(key, new ArrayList<>());
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            System.out.println(queue);
+            Set<String> row = new HashSet<>();
+            for (int i = 0; i < size; i++) {
+                String s = queue.poll();
+                List<String> nbs = getNbs(s, set);
+                for (String str : nbs) {
+                    map.get(str).add(s);
+                    if (!row.contains(str)){
+                        queue.add(str);
+                        row.add(str);
+                    }
+                }
+            }
+            set.removeAll(row);
+        }
     }
     
-    private List<List<String>> bfsHelper (List<String> currentTransaction, String endWord, Set<String> wordList, Set<String> transitionWords) {
-    	List<List<String>> res = new ArrayList<>();
-
-    	
-    	String originalWord = currentTransaction.get(currentTransaction.size() - 1);
-    	wordList.remove(originalWord);
-    	char[] wordChars = originalWord.toCharArray();
-    	
-    	for (int i = 0; i < wordChars.length; i++) {
-    		char originalChar = wordChars[i];
-    		
-    		for (char c = 'a'; c <= 'z'; c++) {
-    			wordChars[i] = c;
-    			String newWord = String.valueOf(wordChars);
-    			
-    			if (wordList.contains(newWord)) {
-    				transitionWords.add(newWord);
-    				currentTransaction.add(newWord);
-    				res.add(new ArrayList<>(currentTransaction));
-    				currentTransaction.remove(currentTransaction.size() - 1); // back track
-    			}
-    			
-    			if (newWord.equals(endWord)) {
-    				currentTransaction.add(newWord);
-    				res.clear();
-    				res.add(new ArrayList<>(currentTransaction));
-    				return res;
-    			}
-    		}
-    		wordChars[i] = originalChar; // back track
-    	}
-    			return res;
-    	
+    List<String> getNbs(String word, Set<String> set) {
+        List<String> list = new ArrayList<>();
+        char[] chars = word.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = word.charAt(i);
+            for (char a = 'a'; a <= 'z'; a++) {
+                if (a == c) {
+                    continue;
+                }
+                chars[i] = a;
+                String temp = new String(new String(chars));
+                if (set.contains(temp)) {
+                    list.add(temp);
+                }
+            }
+            chars[i] = c;
+        }
+        return list;
     }
 }
