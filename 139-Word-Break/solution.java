2a2e63c3a52b92e0a404560133ec0779a9135131
always414 <yichenzhang14@gmail.com>
2016-07-15 13:29:36 -0700

    [139. Word Break][Accepted]committed by Me

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,30 +1,17 @@
+import java.util.Arrays;
 import java.util.Set;
 
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
-    	if (s == null) {
-    		throw new IllegalArgumentException("Null string input");
-    	}
-    	
-    	if (s.length() == 0) {
-    		return true;
-    	}
-    	
-    	if (s.length() != 0 && wordDict.size() == 0) {
-    		return false;
-    	}
-    	
-    	boolean canBreak = false;
-    	for (int i = 0; i < s.length() + 1; i++) {
-    		if (wordDict.contains(s.substring(0, i))) {
-//    			System.out.println(s.substring(0, i));
-    			canBreak = wordBreak(s.substring(i), wordDict);
-    			if (canBreak) {
-    				return true;
-    			}
-    		}
-    	}
-
-    	return false;
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i < dp.length; i++) {
+        	for (int j = i-1; j >= 0; j--) {
+        		if (dp[j] && wordDict.contains(s.substring(j, i))) {
+        			dp[i] = true;
+        		}
+        	}
+        }
+        return dp[s.length()];
     }
 }
