c29c840306af1a27d8ff7d807b0be04d12887d8c
always414 <yichenzhang14@gmail.com>
2016-07-17 18:15:15 -0700

    [317. Shortest Distance from All Buildings][Runtime Error]committed by Me

--- 317-Shortest-Distance-from-All-Buildings/solution.java
+++ 317-Shortest-Distance-from-All-Buildings/solution.java
@@ -1,39 +1,60 @@
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 
 public class Solution {
     public int shortestDistance(int[][] grid) {
-        int INF = Integer.MAX_VALUE;
-        int width = grid[0].length;
-        int[][] matrix = new int[grid.length][grid[0].length];
-        Queue<Integer> queue = new ArrayDeque<>();
+        if (grid == null || grid[0].length == 0) return 0;
         
-        for (int i = 0; i < grid.length; i++ ) {
-        	for (int j = 0; j < grid[0].length; j++) {
-        		if (grid[i][j] == 0) {
-        			matrix[i][j] = 0;
-        		} else if (grid[i][j] == 1) {
-        			matrix[i][j] = 0;
-        			queue.add(i * width + j);
-        		} else {
-        			matrix[i][j] = INF;
+        final int[] shift = new int[] {0, 1, 0, -1, 0};
+        
+        int row = grid.length, col = grid[0].length;
+        int[][] distance = new int[row][col];
+        int[][] reach = new int[row][col];
+        int buildingNum = 0;
+        
+        for (int i = 0; i < row; i++) {
+        	for (int j = 0; j < col; j++) {
+        		if (grid[i][j] == 1) {
+        			buildingNum ++;
+        			Queue<int[]> queue = new ArrayDeque<>();
+        			queue.offer(new int[]{i, j});
+        			
+        			boolean[][] isVisited = new boolean[row][col];
+        			int level = 1;
+        			
+        			while (!queue.isEmpty()) {
+        				int size = queue.size();
+        				while (size -- > 0) {
+        					int[] curr = queue.poll();
+        					for (int k = 0; i < 4; k++) {
+        						int nextRow = curr[0] + shift[k];
+        						int nextCol = curr[1] + shift[k+1];
+        						
+        						if (nextRow >= 0 && nextRow < row && nextCol >= 0 && nextCol < col
+        								&& grid[nextRow][nextCol] == 0 && isVisited[nextRow][nextCol]) {
+        							distance[nextRow][nextCol] += level;
+        							reach[nextRow][nextCol] ++;
+        							
+        							isVisited[nextRow][nextCol] = true;
+        							queue.offer(new int[] {nextRow, nextCol});
+        							//不会有反向的问题吗？
+        						}
+        					}
+        				}
+        				level ++;
+        			}
+        		}
+        	}
+        }
+        int shortest = Integer.MAX_VALUE;
+        for (int i = 0; i < row; i++) {
+        	for (int j = 0; j < col; j++) {
+        		if (grid[i][j] == 0 && reach[i][j] == buildingNum) {
+        			shortest = Math.min(shortest, distance[i][j]);
         		}
         	}
         }
         
-        while(!queue.isEmpty()) {
-        	int index = queue.poll();
-        	int row = index / width;
-        	int col = index % width;
-        	
-        	if (row + 1 < grid.length && grid[row + 1][col] == 0) {
-        		matrix[row + 1][col] += (matrix[row][col] + 1);
-        	}
-        	
-        }
-        
-        
-        
+        return shortest == Integer.MAX_VALUE? -1 : shortest;
     }
 }
