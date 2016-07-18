c95dbd90a51ebc2f42f997395a0841e23361937d
always414 <yichenzhang14@gmail.com>
2016-07-18 16:14:51 -0700

    [374. Guess Number Higher or Lower][Compile Error]committed by Me

--- 374-Guess-Number-Higher-or-Lower/solution.java
+++ 374-Guess-Number-Higher-or-Lower/solution.java
@@ -9,30 +9,18 @@ public class Solution extends GuessGame {
 
 	public int guessNumber(int n) {
 		int start = 1, end = n;
-		int num = pickRandom(1, n);
-
-		while (true) {
+		while (start + 1 < end) {
+			num = start + (end - start) / 2;
 			if (guess(num) == 1) {
 				start = num;
-				num = pickRandom(num, end);
-			} else if (guess(num) == -1) {
+			} else if (guess(num == -1)) {
 				end = num;
-				num = pickRandom(start, num);
 			} else {
 				return num;
 			}
-			if (num == -1) 
-				break;
 		}
 		
 		return -1;
 
-	}
-
-	private int pickRandom(int start, int end) {
-		if (start < 0 || start > end)
-			return -1;
-		int index = start + (end - start) / 2;
-		return index;
 	}
 }
