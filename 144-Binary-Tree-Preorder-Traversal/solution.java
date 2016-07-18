2d6f5b8f1e8b681128d5cf58f75d6ae2312b4193
always414 <yichenzhang14@gmail.com>
2016-07-17 23:33:55 -0700

    [144. Binary Tree Preorder Traversal][Accepted]committed by Me

--- 144-Binary-Tree-Preorder-Traversal/solution.java
+++ 144-Binary-Tree-Preorder-Traversal/solution.java
@@ -17,15 +17,18 @@ public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
     	List<Integer> res = new ArrayList<>();
     	Deque<TreeNode> stack = new ArrayDeque<>();
+    	if (root == null) return res;
+    	
     	stack.push(root);
     	
     	while (!stack.isEmpty()) {
     		TreeNode node = stack.pop();
-    		if (node == null) continue;
-    		
     		res.add(node.val);
-			stack.push(node.right);
-    		stack.push(node.left);
+    		
+    		if (node.right != null)
+    			stack.push(node.right);
+    		if (node.left != null)
+    			stack.push(node.left);
     	}
     	return res;
     }
