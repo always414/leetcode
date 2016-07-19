d725357b7d29674acc9715827d199fa0bc5ba50d
always414 <yichenzhang14@gmail.com>
2016-07-19 10:59:50 -0700

    [373. Find K Pairs with Smallest Sums][Accepted]committed by Me

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -15,20 +15,19 @@ public class Solution {
 				return sum1.compareTo(sum2);
 			}
 		});
-		
-		for (int i = 0; i < nums1.length; i++) {
-			for (int j = 0; j < nums2.length; j++) {
-				int[] pair = new int[] {nums1[i], nums2[j]};
+		for (int i = 0; i < Math.min(k, nums1.length); i++) {
+			for (int j = 0; j < Math.min(k, nums2.length); j++) {
+				int[] pair = new int[] { nums1[i], nums2[j] };
 				heap.add(pair);
 			}
 		}
-		
+
 		while (k-- > 0) {
 			if (!heap.isEmpty()) {
 				res.add(heap.poll());
 			}
 		}
-		
+
 		return res;
 
 	}
