6fc2cb4832c2c4b64765e57c8567d82ee7911575
always414 <yichenzhang14@gmail.com>
2016-07-23 16:48:09 -0700

    [361. Bomb Enemy][Accepted]committed by Me

--- 361-Bomb-Enemy/solution.java
+++ 361-Bomb-Enemy/solution.java
@@ -1,54 +1,32 @@
-
 public class Solution {
-	public int maxKilledEnemies(char[][] grid) {
-		int[][] row = new int[grid.length][grid[0].length];
-		int[][] col = new int[grid.length][grid[0].length];
-
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[0].length; j++) {
-				
-				if (grid[i][j] == 'W') {
-					continue;
-				}
-				
-				row[i][j] += (j == 0 ? 0 : row[i][j - 1]);
-				row[i][j] += (grid[i][j] == 'E'? 1 : 0);
-			}
-		}
-		
-		for (int j = 0; j < grid[0].length; j++) {
-			for (int i = 0; i < grid.length; i++) {
-				if (grid[i][j] == 'W') {
-					continue;
-				}
-				
-				col[i][j] += (i == 0? 0 : col[i-1][j]);
-				col[i][j] += (grid[i][j] == 'E'? 1 : 0);
-			}
-		}
-		
-		int res = 0;
-		for (int i = grid.length - 1; i >= 0; i--) {
-			for (int j = grid[0].length - 1; j >= 0; j--) {
-				if (j != grid[0].length - 1 && row[i][j] != 0) {
-					row[i][j] = Math.max(row[i][j], row[i][j+1]);
-				}
-				
-				if (i != grid.length - 1 && col[i][j] != 0) {
-					col[i][j] = Math.max(col[i][j], col[i+1][j]);
-				}
-				
-				if (grid[i][j] == 'E') {
-					res = Math.max(res, col[i][j] + row[i][j] - 1);
-				} else { 
-					res = Math.max(res, col[i][j] + row[i][j]);
-				}
-				
-			}
-		}
-
-		return res;
-	}
-
-	
+    public int maxKilledEnemies(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        
+        int result = 0;
+        int rowCache = 0;
+        int[] colCache = new int[grid[0].length];
+        
+        for (int i = 0; i < grid.length; i++) {
+        	for (int j = 0; j < grid[0].length; j++) {
+        		if (j == 0 || grid[i][j-1] == 'W') {
+        			rowCache = 0;
+        			for (int k = j ; k < grid[0].length && grid[i][k] != 'W'; k++) {
+        				rowCache += grid[i][k] == 'E' ? 1 : 0;
+        			}
+        		}
+        		
+        		if (i == 0 || grid[i-1][j] == 'W') {
+        			colCache[j] = 0;
+        			for (int k = i; k < grid.length && grid[k][j] != 'W'; k++) {
+        				colCache[j] += grid[k][j] == 'E' ? 1 : 0;
+        			}
+        		}
+        		
+        		if (grid[i][j] == '0') {
+        			result = Math.max(result, rowCache + colCache[j]);
+        		}
+        	}
+        }
+    	return result;
+    }
 }
