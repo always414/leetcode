16753e066aeb5fde512745dc2c81a7b5ebc8615f
always414 <yichenzhang14@gmail.com>
2016-07-23 17:30:54 -0700

    [376. Wiggle Subsequence][Runtime Error]committed by Me

---
+++ 376-Wiggle-Subsequence/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int wiggleMaxLength(int[] nums) {
+    	if (nums.length < 2) return nums.length;
+
+    	int[] diff = new int[nums.length];
+    	for (int i = 1; i < nums.length; i++) {
+    		if (nums[i] - nums[i-1] == 0) {
+    			diff[i] = 0;
+    		} else if (nums[i] - nums[i-1] > 0) {
+    			diff[i] = 1;
+    		} else {
+    			diff[i] = -1;
+    		}
+    	}
+    	
+    	diff[0] = 0 - diff[1];
+    	
+    	int count = 1;
+    	for (int i = 1; i < nums.length; i++) {
+    		int j = 1;
+    		while (diff[i-j] == 0) {
+    			j++;
+    		}
+    		if (diff[i] * diff[i-1] < 0) {
+    			count ++;
+    		}
+    	}
+       
+    	return count;
+    }
+}
