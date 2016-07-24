9c60d43bb0240bb0ffc572dd35c760f564586148
always414 <yichenzhang14@gmail.com>
2016-07-23 17:37:52 -0700

    [376. Wiggle Subsequence][Accepted]committed by Me

--- 376-Wiggle-Subsequence/solution.java
+++ 376-Wiggle-Subsequence/solution.java
@@ -5,27 +5,20 @@ public class Solution {
     	if (nums.length < 2) return nums.length;
 
     	int[] diff = new int[nums.length];
-    	for (int i = 1; i < nums.length; i++) {
-    		if (nums[i] - nums[i-1] == 0) {
-    			diff[i] = 0;
-    		} else if (nums[i] - nums[i-1] > 0) {
-    			diff[i] = 1;
-    		} else {
-    			diff[i] = -1;
-    		}
-    	}
-    	
+    	diff[1] = nums[1] == nums[0]? 0 : nums[1] - nums[0] > 0? 1 : -1;
     	diff[0] = 0 - diff[1];
-    	
+
     	int count = 1;
+    	
     	for (int i = 1; i < nums.length; i++) {
+    		diff[i] = nums[i] == nums[i-1] ? 0 : nums[i] - nums[i-1] > 0 ? 1 : -1;
     		int j = 1;
     		while ((i-j) > 0 && diff[i-j] == 0) {
     			j++;
     		}
     		if (diff[i] * diff[i-j] < 0) {
     			count ++;
-    		} 
+    		}
     	}
 //       System.out.println(Arrays.toString(diff));
     	return count;
