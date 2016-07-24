1ceb146f3e2bd1411e9641a472a9a45607c2f400
always414 <yichenzhang14@gmail.com>
2016-07-24 08:43:08 -0700

    [364. Nested List Weight Sum II][Accepted]committed by Me

--- 364-Nested-List-Weight-Sum-II/solution.java
+++ 364-Nested-List-Weight-Sum-II/solution.java
@@ -1,73 +1,40 @@
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Deque;
 import java.util.List;
-import java.util.PriorityQueue;
 
 /**
- * // This is the interface that allows for creating nested lists. // You should
- * not implement it, or speculate about its implementation public interface
- * NestedInteger {
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
  *
- * // @return true if this NestedInteger holds a single integer, rather than a
- * nested list. public boolean isInteger();
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
  *
- * // @return the single integer that this NestedInteger holds, if it holds a
- * single integer // Return null if this NestedInteger holds a nested list
- * public Integer getInteger();
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
  *
- * // @return the nested list that this NestedInteger holds, if it holds a
- * nested list // Return null if this NestedInteger holds a single integer
- * public List<NestedInteger> getList(); }
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
  */
 public class Solution {
-	public int depthSumInverse(List<NestedInteger> nestedList) {
-		// dfs, stack
-
-		List<Integer> sumDepth = new ArrayList<Integer>();
-		sumDepth.add(0);
-
-		dfsHelper(nestedList, sumDepth, 1);
-
-		Collections.reverse(sumDepth);
-		sumDepth.add(0, 0);
-		int res = 0;
-		for (int i = 0; i < sumDepth.size(); i++) {
-			res += i * sumDepth.get(i);
-		}
-
-		return res;
-
-	}
-
-	private void dfsHelper(List<NestedInteger> nestedList, List<Integer> sumDepth, int depth) {
-		Deque<NestedInteger> stack = new ArrayDeque<>();
-		stack.addAll(nestedList);
-		while (!stack.isEmpty()) {
-			NestedInteger nest = stack.pop();
-			if (nest.isInteger()) {
-				insertInteger(sumDepth, depth, nest.getInteger());
-			} else {
-				Integer integer = nest.getInteger();
-				List<NestedInteger> nestList = nest.getList();
-				
-				if (integer != null) {
-					insertInteger(sumDepth, depth, integer);
-				}
-				
-				if (nestList != null) {
-					dfsHelper(nestedList, sumDepth, depth + 1);
-				}
-			}
-		}
-		
-	}
-
-	private void insertInteger(List<Integer> sumDepth, int depth, int integer) {
-		while (depth >= sumDepth.size()) {
-			sumDepth.add(0);
-		}
-		sumDepth.set(depth, sumDepth.get(depth) + integer);
-	}
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        int unweighted = 0, weighted = 0;
+    	while (! nestedList.isEmpty()) {
+    		List<NestedInteger> nextLevel = new ArrayList<>();
+    		for (NestedInteger ni : nestedList) {
+    			if (ni.isInteger()) 
+    				unweighted += ni.getInteger();
+    			else
+    				nextLevel.addAll(ni.getList());
+    		}
+    		
+    		weighted += unweighted;
+    		nestedList = nextLevel;
+    	}
+    	
+    	return weighted;
+    	
+    }
 }
