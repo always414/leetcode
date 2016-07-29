d0e5a67f4842a85530c43ec84e28d9046944bc23
always414 <yichenzhang14@gmail.com>
2016-07-29 13:43:05 -0700

    [106. Construct Binary Tree from Inorder and Postorder Traversal][Accepted]committed by Me

--- 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.java
+++ 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.java
@@ -6,33 +6,32 @@ import javax.swing.text.Position;
  */
 public class Solution {
 	public TreeNode buildTree(int[] inorder, int[] postorder) {
-		// inorder ... left root right ...
-		// postorder left... right... root
-		
-		TreeNode root = buildHelper(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
-
-		return root;
-	}
-
-	private TreeNode buildHelper(int[] inorder, int[] postorder, int inorderStart, int inorderEnd, int postorderStart,
-			int postorderEnd) {
-		
-		if (inorderStart == inorderEnd && postorderStart == postorderEnd) {
-			return new TreeNode(inorder[inorderStart]);
-		} else if (inorderStart > inorderEnd && postorderStart > postorderEnd) {
+		// inorder ... left ... root ... right ...
+		// postorder ...left ...right root
+		if (inorder == null || postorder == null || inorder.length != postorder.length || inorder.length == 0) {
 			return null;
 		}
+
+		TreeNode root = buildHelper(inorder, 0, inorder.length -1, postorder, postorder.length -1);
+		return root;
+	}
+
+	private TreeNode buildHelper(int[] inorder, int start, int end, int[] postorder, int rootIndex) {
+		if (start > end || rootIndex < 0 || rootIndex > postorder.length - 1) return null;
+		if (start == end) return new TreeNode(inorder[start]);
 		
-		int inorderRoot = postorderEnd;
-		while (inorderRoot >= 0 && inorder[inorderRoot] != postorder[postorderEnd]) {
-			inorderRoot --;
+		TreeNode root = new TreeNode(postorder[rootIndex]);
+		
+		int index = end;
+		while (index > start && inorder[index] != root.val) {
+			index --;
 		}
 		
-		TreeNode root = new TreeNode(inorder[inorderRoot]);
-		root.left = buildHelper(inorder, postorder, inorderStart, inorderRoot - 1, inorderStart, inorderRoot - 1);
-		root.right = buildHelper(inorder, postorder, inorderRoot + 1, inorderEnd, inorderRoot, postorderEnd - 1);
+		root.left = buildHelper(inorder, start, index - 1, postorder, rootIndex - end + index - 1);
+		root.right = buildHelper(inorder, index + 1,  end, postorder, rootIndex - 1);
 		
 		return root;
-		
 	}
+	
+	
 }
