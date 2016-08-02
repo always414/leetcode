9028b02ccffaa32bfc6d501d17f0b3bfa4593742
always414 <yichenzhang14@gmail.com>
2016-08-01 18:10:19 -0700

    [297. Serialize and Deserialize Binary Tree][Accepted]committed by Me

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.java
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.java
@@ -1,87 +1,54 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Queue;
+import java.util.Deque;
 
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 public class Codec {
+	private static String NULL = "null";
+	private static String DELIMITER = ",";
+
+	// Encodes a tree to a single string.
+	public String serialize(TreeNode root) {
+		StringBuilder sb = new StringBuilder();
+		buildString(root, sb);
+		return sb.toString();
+	}
+
+	private void buildString(TreeNode root, StringBuilder sb) {
+		if (root == null) {
+			sb.append(NULL).append(DELIMITER);
+		} else {
+			sb.append(root.val).append(DELIMITER);
+			buildString(root.left, sb);
+			buildString(root.right, sb);
+		}
+	}
+
+	// Decodes your encoded data to tree.
+	public TreeNode deserialize(String data) {
+		if (data == null || data.length() == 0 || !data.contains(","))
+			return null;
+		Deque<String> nodes = new ArrayDeque<>();
+		nodes.addAll(Arrays.asList(data.split(DELIMITER)));
+
+		return buildTree(nodes);
+	}
 	
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        //use bfs to construct a string
-    	StringBuilder data = new StringBuilder();
-    	if (root == null) return data.toString();
-    	
-    	Queue<TreeNode> queue = new ArrayDeque<>();
-    	queue.add(root);
-    	StringBuilder currentLevel = new StringBuilder();
-    	currentLevel.append(root.val + ",");
-    	while (!queue.isEmpty() && currentLevel.length() > 0) {
-    		StringBuilder nextLevel = new StringBuilder();
-    		String[] nodes = currentLevel.toString().split(",");
-    		for (int i = 0; i < nodes.length; i++) {
-    			if (nodes[i].equals("null")) {
-    				nextLevel.append("null,null,");
-    			} else {
-    				TreeNode node = queue.poll();
-    				if (node.left != null) {
-    					queue.offer(node.left);
-    					nextLevel.append(node.left.val + ",");
-    				} else {
-    					nextLevel.append("null,");
-    				}
-    				
-    				if (node.right != null) {
-    					queue.offer(node.right);
-    					nextLevel.append(node.right.val + ",");
-    				} else {
-    					nextLevel.append("null,");
-    				}
-    			}
-    		}
-    		data.append(currentLevel.toString());
-    		currentLevel = nextLevel;
-    	}
-    	return data.toString();
-    }
+	private TreeNode buildTree(Deque<String> nodes) {
+		String val = nodes.remove();
+		if (val.equals(NULL)) return null;
+		else {
+			TreeNode node = new TreeNode(Integer.parseInt(val));
+			node.left = buildTree(nodes);
+			node.right = buildTree(nodes);
+			return node;
+		}
+	}
 
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-    	if (data == null || data.length() == 0 || !data.contains(",")) {
-    		return null;
-    	}
-    	String[] array = data.split(",");
-    	TreeNode[] serial = new TreeNode[array.length];
-    	serial[0] = generateNode(array, serial, 0);
-    	for (int i = 0; i < array.length; i++) {
-    		if (serial[i] == null) continue;
-    		serial[i].left = generateNode(array,serial, 2*i + 1);
-    		serial[i].right = generateNode(array,serial, 2 * i + 2);
-    	}
-    	return serial[0];
-    }
-    
-    private TreeNode generateNode(String[] array, TreeNode[] serial, int index) {
-    	if (index > array.length - 1 || array[index].equals("null")) {
-    		return null;
-    	} else if (serial[index] != null) {
-    		return serial[index];
-    	} else {
-    		serial[index] = new TreeNode(Integer.parseInt(array[index]));
-    		return serial[index];
-    	}
-    }
 }
-
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
