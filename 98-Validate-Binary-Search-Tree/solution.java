f441f45875aafaa31adcf70087a7a931f020a429
always414 <yichenzhang14@gmail.com>
2016-08-03 12:18:04 -0700

    [98. Validate Binary Search Tree][Accepted]committed by Me

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -1,36 +1,19 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        
-        if (root.left.val >= root.val || root.right.val <= root.val) return false;
-        
-        return max(root.left) < root.val && min(root.right) > root.val && isValidBST(root.left) && isValidBST(root.right);
-    }
-    
-    private int max (TreeNode root) {
-    	if (root == null) return Integer.MIN_VALUE;
-    			
-    	while (root.right != null) {
-    		root = root.right;
-    	}
-    	
-    	return root.val;
-    }
-    
-    private int min(TreeNode root) {
-    	if (root == null) return Integer.MAX_VALUE;
-    	while (root.left != null) {
-    		root = root.left;
-    	}
-    	return root.val;
-    }
+
+	public boolean isValidBST(TreeNode root) {
+		return isvalidBST(root, Long.MAX_VALUE, Long.MIN_VALUE);
+	}
+
+	private boolean isvalidBST(TreeNode root, long upperBound, long lowerBound) {
+		if (root == null)
+			return true;
+		if (root.val >= upperBound || root.val <= lowerBound) {
+			return false;
+		}
+		return isvalidBST(root.left, root.val, lowerBound) && isvalidBST(root.right, upperBound, root.val);
+	}
 }
