cc9a4f66d81b1284a25602cb91b1483085701a58
always414 <yichenzhang14@gmail.com>
2016-08-03 11:29:38 -0700

    [298. Binary Tree Longest Consecutive Sequence][Accepted]committed by Me

--- 298-Binary-Tree-Longest-Consecutive-Sequence/solution.java
+++ 298-Binary-Tree-Longest-Consecutive-Sequence/solution.java
@@ -1,17 +1,32 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
 public class Solution {
-    private int max = 0;
-    public int longestConsecutive(TreeNode root) {
-        if(root == null) return 0;
-        helper(root, 0, root.val);
-        return max;
-    }
-    
-    public void helper(TreeNode root, int cur, int target){
-        if(root == null) return;
-        if(root.val == target) cur++;
-        else cur = 1;
-        max = Math.max(cur, max);
-        helper(root.left, cur, root.val + 1);
-        helper(root.right, cur, root.val + 1);
-    }
+	private int longest = 0;
+
+	public int longestConsecutive(TreeNode root) {
+		helper(root);
+		return longest;
+	}
+
+	private int helper(TreeNode root) {
+		if (root == null)
+			return 0;
+
+		int left = helper(root.left);
+		int right = helper(root.right);
+
+		int length = 1;
+		if (root.left != null && root.left.val == root.val + 1) {
+			length = Math.max(left + 1, length);
+		}
+
+		if (root.right != null && root.right.val == root.val + 1) {
+			length = Math.max(right + 1, length);
+		}
+		longest = Math.max(length, longest);
+
+		return length;
+	}
 }
