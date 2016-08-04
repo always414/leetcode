222d8d56988613de5490b136477029c12c18a3c4
always414 <yichenzhang14@gmail.com>
2016-08-04 13:36:24 -0700

    [333. Largest BST Subtree][Accepted]committed by Me

--- 333-Largest-BST-Subtree/solution.java
+++ 333-Largest-BST-Subtree/solution.java
@@ -1,56 +1,44 @@
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
 public class Solution {
-	int res = 0;
-	public int largestBSTSubtree(TreeNode root) {
-		dfs(root);
-		return res;
-	}
-
-     /**
-      * dfs search each node from root to leaf, and if find the largest subtree, end recursion.
-      * @param root
-      */
-	private void dfs(TreeNode root) {
-		if (root == null)
-			return;
-		int d = largestBSTSubtree(root, Long.MAX_VALUE, Long.MIN_VALUE);
-
-		if (d != -1) {
-			res = Math.max(d, res);
-			return;
+	class Result {
+		int size;
+		int max;
+		int min;
+		public Result(int size, int min, int max) {
+			this.size = size;
+			this.max = max;
+			this.min = min;
 		}
-
-		dfs(root.left);
-		dfs(root.right);
 	}
+	
+	
+    public int largestBSTSubtree(TreeNode root) {
+    	Result res = BSTSubtree(root);
+    	return Math.abs(res.size);
+    	
+    }
+    
+    private Result BSTSubtree(TreeNode root) {
+    	if (root == null) return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    	Result left = BSTSubtree(root.left);
+    	Result right = BSTSubtree(root.right);
+    	
+    	if (left.size < 0 || right.size < 0 || root.val < left.max || root.val > right.min) {
+    		return new Result(Math.max(Math.abs(left.size), Math.abs(right.size)) * (-1), 0, 0);
+    	} else {
+    		return new Result(left.size + right.size + 1, Math.min(root.val, left.min), Math.max(root.val, right.max));
+    	}
+    	
+    }
+    
+   
 
-	/**
-	 * Returns the largestBSTTree of root and update global max value
-	 * 
-	 * @param root
-	 * @param upperbound
-	 * @param lowerbound
-	 * @return -1 if root is not in valid range, or subtree of root is not valid
-	 *         BST; 0 if root is null; tree size of root if it's a valid bst
-	 */
-	private int largestBSTSubtree(TreeNode root, long upperbound, long lowerbound) {
-		if (root == null)
-			return 0;
-		if (root.val < lowerbound || root.val > upperbound) {
-			return -1;
-		}
-
-		int left = largestBSTSubtree(root.left, root.val, lowerbound);
-		if (left == -1)
-			return -1;
-
-		int right = largestBSTSubtree(root.right, upperbound, root.val);
-		if (right == -1)
-			return -1;
-
-		return left + right + 1;
-	}
 }
