6c08a2c44029fdf8f518d9aba1a9435c74d4a38a
always414 <yichenzhang14@gmail.com>
2016-08-05 16:56:33 -0700

    [132. Palindrome Partitioning II][Accepted]committed by Me

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -1,36 +1,23 @@
 import java.util.Arrays;
 
 public class Solution {
-	//dfs or dynamic programming
+	// dfs or dynamic programming
 	public int minCut(String s) {
-		int[] matrix = new int[s.length() + 1];
-		Arrays.fill(matrix, Integer.MAX_VALUE);
-		matrix[0] = 0;
-		
-		for (int i = 1; i < matrix.length; i++) {
-			for (int j = 0; j < i; j++) {
-				if (matrix[j] == Integer.MAX_VALUE || matrix[j] + 1 >= matrix[i]) continue;
-				
-				if (isPalindrome(s.substring(j, i))) {
-					matrix[i] = Math.min(matrix[j] + 1, matrix[i]);
-//					break;
+		char[] c = s.toCharArray();
+		int[] cut = new int[s.length()];
+		boolean[][] pal = new boolean[s.length()][s.length()];
+
+		for (int i = 0; i < cut.length; i++) {
+			int min = i;
+			for (int j = 0; j <= i; j++) {
+				if (c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+					pal[j][i] = true;
+					min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
 				}
 			}
+			cut[i] = min;
 		}
-		return matrix[matrix.length - 1] - 1;
+		return cut[cut.length - 1];
 	}
 
-	private boolean isPalindrome(String s) {
-		if (s == null || s.length() == 0)
-			return false;
-		if (s.length() == 1)
-			return true;
-		
-		char[] chars = s.toCharArray();
-		for (int i = 0, j = chars.length - 1; i <= j; i++, j--) {
-			if (chars[i] != chars[j])
-				return false;
-		}
-		return true;
-	}
 }
