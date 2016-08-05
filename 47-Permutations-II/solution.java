dfd352d72348bbc0e1367a9db73b64182f321085
always414 <yichenzhang14@gmail.com>
2016-08-04 17:06:37 -0700

    [47. Permutations II][Accepted]committed by Me

---
+++ 47-Permutations-II/solution.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+	public List<List<Integer>> permuteUnique(int[] nums) {
+		Set<List<Integer>> res = new HashSet<>();
+		List<Integer> sub = new ArrayList<>();
+		for (int i : nums) {
+			sub.add(i);
+		}
+		permuteHelper(res, sub, 0);
+		return new ArrayList<>(res);
+	}
+
+	private void permuteHelper(Set<List<Integer>> result, List<Integer> sub, int start) {
+		if (start > sub.size())
+			return;
+
+		if (start == sub.size()) {
+			result.add(new ArrayList<>(sub));
+		}
+
+		for (int i = start; i < sub.size(); i++) {
+			Collections.swap(sub, start, i);
+			permuteHelper(result, sub, start + 1);
+			Collections.swap(sub, start, i);
+		}
+	}
+}
