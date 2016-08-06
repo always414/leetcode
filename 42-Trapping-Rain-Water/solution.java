84a4f75d46c69e713bd8a00908cb4710b110adc1
always414 <yichenzhang14@gmail.com>
2016-08-06 09:52:30 -0700

    [42. Trapping Rain Water][Accepted]committed by Me

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -1,5 +1,32 @@
 public class Solution {
-    public int trap(int[] height) {
-        
-    }
+	public int trap(int[] height) {
+		int highestPoint = 0;
+		int h = 0;
+		int water = 0;
+		for (int i = 0; i < height.length; i++) {
+			if (height[i] < h)
+				continue;
+			h = height[i];
+			highestPoint = i;
+		}
+
+		int left = 0;
+		for (int i = 0; i < highestPoint; i++) {
+			if (height[i] < left) {
+				water += (left - height[i]);
+			} else {
+				left = height[i];
+			}
+		}
+		int right = 0;
+		for (int i = height.length - 1; i > highestPoint; i--) {
+			if (height[i] < right) {
+				water += (right - height[i]);
+			} else {
+				right = height[i];
+			}
+		}
+
+		return water;
+	}
 }
