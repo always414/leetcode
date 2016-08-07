cbb3693b4f5f8150ae9ebd79de42de8f635dc8d9
always414 <yichenzhang14@gmail.com>
2016-08-07 00:22:03 -0700

    [362. Design Hit Counter][Runtime Error]committed by Me

--- 362-Design-Hit-Counter/solution.java
+++ 362-Design-Hit-Counter/solution.java
@@ -1,20 +1,31 @@
+import java.util.*;
+
 public class HitCounter {
 
+	Queue<Integer> queue;
     /** Initialize your data structure here. */
     public HitCounter() {
-        
+        //some data structure
+    	//1. implement queue. return queue.size
+    	queue = new ArrayDeque<>();
+    	
     }
     
     /** Record a hit.
         @param timestamp - The current timestamp (in seconds granularity). */
     public void hit(int timestamp) {
-        
+        //store
+    	queue.add(timestamp);
     }
     
     /** Return the number of hits in the past 5 minutes.
         @param timestamp - The current timestamp (in seconds granularity). */
     public int getHits(int timestamp) {
-        
+        //get counts
+    	while (timestamp - queue.peek() > 300) {
+    		queue.poll();
+    	}
+    	return queue.size();
     }
 }
 
