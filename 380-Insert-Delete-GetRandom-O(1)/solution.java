eff40bf3916a1c4ae1410f0b82aa7a7255277c26
always414 <yichenzhang14@gmail.com>
2016-08-08 16:08:00 -0700

    [380. Insert Delete GetRandom O(1)][Runtime Error]committed by Me

--- 380-Insert-Delete-GetRandom-O(1)/solution.java
+++ 380-Insert-Delete-GetRandom-O(1)/solution.java
@@ -2,37 +2,42 @@ import java.util.*;
 
 public class RandomizedSet {
 
-	List<Integer> cur;
-	Set<Integer> set;
+	List<Integer> list;
+	Map<Integer, Integer> map;
 	Random r;
-    /** Initialize your data structure here. */
-    public RandomizedSet() {
-    	cur = new ArrayList<>();
-    	set = new LinkedHashSet<>();
-    	r = new Random();
-    }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    public boolean insert(int val) {
-        return set.add(val) && cur.add(val);
-    }
-    
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    public boolean remove(int val) {
-    	Integer o = val;
-        return set.remove(o) && cur.remove(o);
-    }
-    
-    /** Get a random element from the set. */
-    public int getRandom() {
-        return cur.isEmpty()? 0 : cur.get(r.nextInt(cur.size()));
-    }
+
+	/** Initialize your data structure here. */
+	public RandomizedSet() {
+		list = new ArrayList<>();
+		map = new LinkedHashMap<>();
+		r = new Random();
+	}
+
+	/**
+	 * Inserts a value to the set. Returns true if the set did not already
+	 * contain the specified element.
+	 */
+	public boolean insert(int val) {
+		return map.putIfAbsent(val, list.size()) == null ? list.add(val) : false;
+	}
+
+	/**
+	 * Removes a value from the set. Returns true if the set contained the
+	 * specified element.
+	 */
+	public boolean remove(int val) {
+		Integer index = map.remove(val);
+		return index == null ? false : list.remove((int) index) != null;
+	}
+
+	/** Get a random element from the set. */
+	public int getRandom() {
+		return list.isEmpty() ? 0 : list.get(r.nextInt(list.size()));
+	}
 }
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
+ * RandomizedSet obj = new RandomizedSet(); boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val); int param_3 = obj.getRandom();
  */
