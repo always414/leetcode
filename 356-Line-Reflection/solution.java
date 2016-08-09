cff609ab1cd9a9f71b2563ed5112138774d0fd77
always414 <yichenzhang14@gmail.com>
2016-08-09 00:41:17 -0700

    [356. Line Reflection][Runtime Error]committed by Me

--- 356-Line-Reflection/solution.java
+++ 356-Line-Reflection/solution.java
@@ -1,26 +1,37 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
 public class Solution {
 	public boolean isReflected(int[][] points) {
-		if (points == null || points.length < 2 || points[0].length < 2) {
+		if (points == null || points.length < 2) {
 			return true;
 		}
 		// find y
+		Map<Double, Integer> map = new HashMap<>();
 		int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE;
 		for (int i = 0; i < points.length; i++) {
+			map.put((double) points[i][0], points[i][1]);
 			if (points[i][0] < minX) {
 				minX = points[i][0];
-			} 
+			}
 			if (points[i][0] > maxX) {
 				maxX = points[i][0];
 			}
 		}
-		
-		double y = (minX + maxX) / 2;
-		
-		double dist = 0;
+		double y = (minX + maxX) / 2.0;
 		for (int i = 0; i < points.length; i++) {
-			dist ^= Math.abs(y - points[i][0]);
-			dist ^= points[i][1];
+			if (!map.containsKey((double)points[i][0])) continue;
+			double x2 = 2 * y - points[i][0];
+			if (map.get(x2) == points[i][1]) {
+				map.remove(x2);
+				map.remove((double)points[i][0]);
+			} else {
+				return false;
+			}
 		}
-		return dist == 0;
+		return map.isEmpty();
 	}
 }
