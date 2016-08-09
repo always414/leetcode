3eaec4e27d40e14c8ae26257ae9f5a197009f15a
always414 <yichenzhang14@gmail.com>
2016-08-09 12:28:57 -0700

    [271. Encode and Decode Strings][Accepted]committed by Me

--- 271-Encode-and-Decode-Strings/solution.java
+++ 271-Encode-and-Decode-Strings/solution.java
@@ -1,21 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Codec {
-	
+    
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-    	StringBuilder sb = new StringBuilder();
-    	for (String str : strs) {
-    		sb.append(sb.length()).append('/').append(str);
-    	}
-    	
-    	return sb.toString();
-        
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length()).append('/').append(s);
+        }
+        return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
-     public List<String> decode(String s) {
+    public List<String> decode(String s) {
         List<String> ret = new ArrayList<String>();
         int i = 0;
         while(i < s.length()) {
@@ -27,7 +22,3 @@ public class Codec {
         return ret;
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.decode(codec.encode(strs));
