d752c7695387b8665268f3b394e7f92c43aaf05e
always414 <yichenzhang14@gmail.com>
2016-08-09 22:38:21 -0700

    [377. Combination Sum IV][Accepted]committed by Me

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -1,23 +1,24 @@
+import java.util.Arrays;
+
 public class Solution {
-	private int count = 0;
-
+	//recursion depth too large
+	//try dp
 	public int combinationSum4(int[] nums, int target) {
-		helper(nums, target);
-		return count;
-	}
-
-	private void helper(int[] nums, int target) {
-		if (target < 0) {
-			return;
-		}
-
-		for (int i = 0; i < nums.length; i++) {
-			if (target == nums[i]) {
-				count ++;
-			} else if (target - nums[i] > 0){
-				helper(nums, target - nums[i]);
+		
+		int[] dp = new int[target + 1];
+		for (int i = 0; i < dp.length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				if (i == nums[j]) {
+					dp[i] ++;
+				} else if (i - nums[j] > 0) {
+					dp[i] += dp[i - nums[j]];
+				}
 			}
 		}
+		return dp[target];
 	}
+
+	
+
 
 }
