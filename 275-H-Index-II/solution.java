f451e0ae5b5051239863ed9b06db0726e971b5d5
always414 <yichenzhang14@gmail.com>
2016-08-09 18:28:48 -0700

    [275. H-Index II][Time Limit Exceeded]committed by Me

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -1,24 +1,32 @@
 
 public class Solution {
 	public int hIndex(int[] citations) {
+	    if (citations == null || citations.length == 0) return 0;
+	    if (citations.length == 1) {
+	        if (citations[0] == 0) {
+	            return 0;
+	        } else {
+	            return 1;
+	        }
+	    }
 		int hIndex = 0;
 		int len = citations.length;
 		int start = 0, end = citations.length - 1;
 
-		while (start + 1 < end) {
+		while (start <= end) {
 			int mid = start + (end - start) / 2;
-			int min = Math.min(citations[mid], len - mid);
-			if (min >= hIndex) {
-				hIndex = min;
-				start = mid;
+			if (citations[mid] == len - mid) {
+				return len-mid;
+			}
+			
+			if (citations[mid] < len - mid) {
+				start = mid + 1;
 			} else {
 				end = mid;
 			}
 		}
 
-		hIndex = Math.max(Math.max(Math.min(citations[start], len - start), Math.min(citations[end], len - end)),
-				hIndex);
-		return hIndex;
+		return len - start;
 	}
 
 }
