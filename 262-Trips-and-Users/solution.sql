dedff0d810e16e4784390e97d8eee3f037a7b251
always414 <yichenzhang14@gmail.com>
2016-08-13 13:50:20 -0700

    [262. Trips and Users][Runtime Error]committed by Me

--- 262-Trips-and-Users/solution.sql
+++ 262-Trips-and-Users/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-select t.Request_at as Day, round(sum(case when t.Status like 'cancelled_%' then 1 else 0)/count(*), 2) as Rate
+select t.Request_at as Day, round(sum(case when t.Status like 'cancelled_%' then 1 else 0)/count(*)), 2) as Rate
 from Trips t
 inner join users u
 on t.client_id = u.users_id and u.Banned = 'No'
