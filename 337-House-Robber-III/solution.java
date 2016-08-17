c377a69fd23720f95f27a6116411ecfa11bf2cbb
always414 <yichenzhang14@gmail.com>
2016-08-17 16:15:23 -0700

    [337. House Robber III][Accepted]committed by Me

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -11,11 +11,11 @@ public class Solution {
 	private int dfs(TreeNode root) {
 		if (root == null)
 			return 0;
-		int leftGrand = dfs(root.left);
-		int rightGrand = dfs(root.right);
-		
+		int leftGrand = root.left == null? 0 : dfs(root.left);
+		int rightGrand = root.right == null? 0 : dfs(root.right);
 		int left = root.left == null ? 0 : root.left.val;
 		int right = root.right == null ? 0 : root.right.val;
+		
 		root.val = Math.max(left + right, root.val + leftGrand + rightGrand);
 
 		return left + right;
