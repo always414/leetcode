ad88aab244af28af518774a76dadc6bcedbe051d
always414 <yichenzhang14@gmail.com>
2016-08-17 16:14:05 -0700

    [337. House Robber III][Accepted]committed by Me

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -1,69 +1,23 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
-
 /**
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
  * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 public class Solution {
-	private static TreeNode NULLNODE = new TreeNode(0);
-
 	public int rob(TreeNode root) {
-		List<Integer> array = new ArrayList<>();
-		treeToArray(array, root);
-		Integer[] res = new Integer[array.size()];
-		array.toArray(res);
-		for (int i = res.length - 1; i >= 0; i--) {
-			int parent = (i - 1) / 2;
-			if (parent > 0) {
-				int grandParent = (parent - 1) / 2;
-				res[grandParent] += res[i];
-			}
-			
-			int left = 2 * i + 1;
-			int right = 2 * i + 2;
-			if (left < res.length && right < res.length) {
-				res[i] = Math.max(res[left] + res[right], res[i]);
-			}
-		}
-
-		return res[0];
+		dfs(root);
+		return root == null? 0 : root.val;
 	}
-	// recursion logic:
-	// f(root) = max(f(root.l) + f(root.r), root.val + f(root.l.l) + f(root.l.r)
-	// + f(root.r.l) + f(root.r.r))
 
-	private void treeToArray(List<Integer> array, TreeNode root) {
-		Queue<TreeNode> queue = new ArrayDeque<>();
-		queue.add(root);
+	private int dfs(TreeNode root) {
+		if (root == null)
+			return 0;
+		int leftGrand = dfs(root.left);
+		int rightGrand = dfs(root.right);
+		
+		int left = root.left == null ? 0 : root.left.val;
+		int right = root.right == null ? 0 : root.right.val;
+		root.val = Math.max(left + right, root.val + leftGrand + rightGrand);
 
-		while (!queue.isEmpty()) {
-			int size = queue.size();
-			int num = size * 2;
-			while (size-- > 0) {
-				TreeNode node = queue.poll();
-				array.add(node.val);
-
-				if (node.left != null) {
-					queue.add(node.left);
-				} else {
-					queue.add(NULLNODE);
-					num--;
-				}
-
-				if (node.right != null) {
-					queue.add(node.right);
-				} else {
-					queue.add(NULLNODE);
-					num--;
-				}
-			}
-			if (num == 0) {
-				break;
-			}
-		}
+		return left + right;
 	}
 }
