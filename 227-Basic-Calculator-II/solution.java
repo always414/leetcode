9e9b4c889b1dfc9b74ba4f3d119d16fd4fe89c82
always414 <yichenzhang14@gmail.com>
2016-08-18 14:43:04 -0700

    [227. Basic Calculator II][Accepted]committed by Me

--- 227-Basic-Calculator-II/solution.java
+++ 227-Basic-Calculator-II/solution.java
@@ -4,71 +4,42 @@ import java.util.Deque;
 import java.util.HashSet;
 
 public class Solution {
-	static HashSet<Character> operator;
-	static {
-		operator = new HashSet<>();
-		operator.add('+');
-		operator.add('-');
-		operator.add('*');
-		operator.add('/');
-	}
-
 	public int calculate(String s) {
-		ArrayList<String> expression = new ArrayList<>();
-		int numstart = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == ' ') {
-				if (!s.substring(numstart, i).isEmpty()) {
-					expression.add(s.substring(numstart, i));
-				}
-				numstart = i + 1;
-			} else if (operator.contains(s.charAt(i))) {
-				expression.add(s.substring(numstart, i));
-				expression.add("" + s.charAt(i));
-				numstart = i + 1;
-			} else if (i == s.length() - 1) {
-				expression.add(s.substring(numstart, i + 1));
+		if (s == null) return 0;
+		s = s.trim().replaceAll(" +", "");
+		int length = s.length();
+		
+		int res = 0;
+		long preVal = 0;
+		char sign = '+';
+		int i = 0; 
+		
+		while ( i < length) {
+			long curVal = 0;
+			while (i < length && (int) s.charAt(i) <= 57 && (int)s.charAt(i) >= 48) {
+				curVal = curVal * 10 + (s.charAt(i) - '0');
+				i++;
+			}
+			
+			if (sign == '+') {
+				res += preVal;
+				preVal = curVal;
+			} else if (sign == '-') {
+				res += preVal;
+				preVal = -curVal;
+			} else if (sign == '*') {
+				preVal = preVal * curVal;
+			} else if (sign == '/') {
+				preVal = preVal / curVal;
+			}
+			
+			if (i < length) {
+				sign = s.charAt(i);
+				i++;
 			}
 		}
-
-		// first round, calculate * and /
-		for (int i = 0; i < expression.size() && expression.size() > 1; i++) {
-			if (expression.get(i).isEmpty()) {
-				expression.remove(i);
-				i--;
-				continue;
-			}
-
-			if (expression.get(i).equals("*") || expression.get(i).equals("/")) {
-				Integer left = Integer.parseInt(expression.get(i - 1));
-				boolean multiple = expression.get(i).equals("*");
-				Integer right = Integer.parseInt(expression.get(i + 1));
-				Integer res = multiple ? left * right : left / right;
-
-				expression.remove(i + 1);
-				expression.remove(i);
-				expression.remove(i - 1);
-				i--;
-				expression.add(i, "" + res);
-			}
-		}
-
-		// second round, calculate + and -
-		for (int i = 0; i < expression.size() && expression.size() > 1; i++) {
-			if ("+-".contains(expression.get(i))) {
-				Integer left = Integer.parseInt(expression.get(i - 1));
-				boolean plus = expression.get(i).equals("+");
-				Integer right = Integer.parseInt(expression.get(i + 1));
-				Integer res = plus ? left + right : left - right;
-
-				expression.remove(i + 1);
-				expression.remove(i);
-				expression.remove(i - 1);
-				i--;
-				expression.add(i, "" + res);
-			}
-		}
-
-		return Integer.parseInt(expression.get(0));
+		
+		res += preVal;
+		return res;
 	}
 }
