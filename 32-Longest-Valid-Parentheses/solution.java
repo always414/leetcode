a42875a59f5ca93cec6f2a5b1a2ff3f8fac22170
always414 <yichenzhang14@gmail.com>
2016-08-25 13:04:02 -0700

    [32. Longest Valid Parentheses][Time Limit Exceeded]committed by Me

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,30 +1,34 @@
+import java.util.Arrays;
+
 public class Solution {
-    public int longestValidParentheses(String s) {
-        int[] dp = new int[s.length()];
-        int max = 0;
-        dp[0] = 0;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == ')') {
-               int prev = i-1;
-               int sub = 0;
-               while (prev >= 0) {
-                   if (s.charAt(prev) == ')' ) {
-                        if (dp[prev] == 0) break;
-                       sub += dp[prev];
-                       prev -= dp[prev];
-                   } else {
-                       dp[i] += (sub+2);
-                       prev -=1;
-                       sub = 0;
-                        if (prev >= 0 && s.charAt(prev) == '(') break;
-                   }
-               }
-               
-               dp[i] += sub;
-            } 
-            max = Math.max(max, dp[i]);
-        }  
-        
-        return max;
-    }
+	public int longestValidParentheses(String s) {
+		if (s.length() == 0)
+			return 0;
+		int[] dp = new int[s.length()];
+		int max = 0;
+		dp[0] = 0;
+		for (int i = 1; i < s.length(); i++) {
+			
+			if (s.charAt(i) == '(') {
+				if (s.charAt(i-1) == ')') {
+					dp[i] += dp[i-1];
+				} 
+			} else {
+				if (s.charAt(i-1) == '(') {
+					dp[i] = dp[i-1] + 2;
+				} else {
+					int j = i - 1 - dp[i-1];
+					if (j >= 0 && s.charAt(j) == '(') {
+						dp[i] = dp[j] + 2 + dp[i-1];
+ 					}
+				}
+			}
+			max = Math.max(max, dp[i]);
+			
+		}
+		
+		System.out.println(Arrays.toString(dp));
+		return max;
+
+	}
 }
