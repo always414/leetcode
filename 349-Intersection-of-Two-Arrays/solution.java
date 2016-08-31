f8666e9143a487e7ffb1030be3d81ad6f64a1105
always414 <yichenzhang14@gmail.com>
2016-08-30 21:20:54 -0700

    [349. Intersection of Two Arrays][Accepted]committed by Me

--- 349-Intersection-of-Two-Arrays/solution.java
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums1.length; i++) {
-            set.add(nums[i]);
+            set.add(nums1[i]);
         }
         
         Set<Integer> list = new HashSet<>();
