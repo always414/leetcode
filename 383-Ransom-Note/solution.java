ec23770f2ad926337fd7553621e11ed147d44b2a
always414 <yichenzhang14@gmail.com>
2016-08-30 21:44:39 -0700

    [383. Ransom Note][Accepted]committed by Me

--- 383-Ransom-Note/solution.java
+++ 383-Ransom-Note/solution.java
@@ -1,15 +1,35 @@
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        while (magazine.length() > 0) {
-            if (magazine.startsWith(ransomNote) {
-                return true;
-            } else {
-                magazine = magazine.subString(1);
+        if (ransomNote == null || magazine == null) return false;
+        if (ransomNote.equals(magazine)) return true;
+        
+        Map<Character, Integer> chars = new HashMap<>();
+        for (int i = 0; i < ransomNote.length(); i++) {
+            Character c = ransomNote.charAt(i);
+            chars.putIfAbsent(c, 0);
+            
+            Integer freq = chars.get(c);
+            chars.put(c, freq + 1);
+        }
+        
+        for (int i = 0 ; i < magazine.length(); i++) {
+            Character c = magazine.charAt(i);
+            Integer freq = chars.get(c);
+            if (freq != null && freq > 0) {
+                chars.put(c, freq - 1);
             }
         }
         
-        return false;
-        
-        
+
+        Iterator<Integer> valueItr = chars.values().iterator();
+        while (valueItr.hasNext()) {
+            Integer i = valueItr.next();
+            if (i != 0) {
+                return false;
+            }
+        }
+       
+        return true;
+       
     }
 }
