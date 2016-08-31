c6d5085904a7e09768f437594fb13752b190c217
always414 <yichenzhang14@gmail.com>
2016-08-30 21:20:27 -0700

    [349. Intersection of Two Arrays][Compile Error]committed by Me

---
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums[i]);
+        }
+        
+        Set<Integer> list = new HashSet<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                list.add(nums2[i]);
+            }
+        }
+        
+        int[] res = new int[list.size()];
+        Iterator<Integer> itr = list.iterator();
+        for (int i = 0; i < res.length; i++) {
+            res[i] = itr.next();
+        }
+        
+        return res;
+    }
+}
